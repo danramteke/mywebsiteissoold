86f2e4a23262666b70f0562ed677d34a3e956a73
Daniel R <danramteke@gmail.com>
2012-04-23 17:55:32 +0000

    imported mako template

--- build.py
+++ build.py
@@ -1,6 +1,14 @@
 import yaml
+import mako
+from mako.template import Template
+
 with open('data.yml') as data: 
     tweets = yaml.load(data.read())['tweets']
 
-
-print tweets
+#print tweets
+print Template("""
+here are some tweets:
+% for tweet in tweets:
+- ${tweet['text']}
+% endfor
+""").render(tweets=tweets)
