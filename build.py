32d029d301aaa3d28c67da016b63fc78f70ad179
Daniel R <danramteke@gmail.com>
2012-04-24 18:10:08 +0000

    styles

--- build.py
+++ build.py
@@ -8,21 +8,58 @@ with open('data.yml') as data:
 #print tweets
 print Template("""
 <html><head><title>My Website is so Old</title>
+<style type="text/css">
+body, li, a, div {
+  font-family: Helvetica, sans-serif;
+}
+body {
+margin-left: auto; margin-right: auto;
+width:90%;
+}
+
+li {
+  margin-top: 5px;
+  margin-bottom: 30px;
+  background-color: #dddddd;
+  padding: 8px;
+}
+
+a{
+display:block;
+font-size: smaller;
+text-align:right;
+}
+
+div.quote {
+background-color: white;
+padding: 8px;
+margin-bottom:8px;
+}
+
+ol {
+list-style-type: none;
+}
+
+h1, h2 {
+display: block;
+text-align: center;
+}
+
+</style>
+</head>
 
 <body>
-
 <h1>My Website is so Old</h1>
 <h2>An Unillustrated Mockularity</h2>
 
-<div id="list_o_tweets">
+<ol id="list_o_tweets">
 % for tweet in tweets:
-  <div class="box">
-    <div class="quote">
-      ${tweet['text']} 
-   </div>
-  </div>
+  <li>
+    <div class="quote">${tweet['text']}</div>
+    <a href="${tweet['url']}">Original</a>
+  </li>
 % endfor
-</div>
+</ol>
 
 </body>
 </html>
